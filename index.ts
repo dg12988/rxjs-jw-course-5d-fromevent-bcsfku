ba4f9af367cfbff738de4eb5b993b25ebe403b12
dg12988 <doug.goldberg@me.com>
2021-08-10 02:14:48 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import { fromEvent } from "rxjs";
 
 const triggerButton = document.querySelector('button#trigger');
 
-fromEvent(triggerButton, 'click').subscribe(
-  event => console.log(event);
+fromEvent<MouseEvent>(triggerButton, 'click').subscribe(
+  event => console.log(event.type, event.x, event.y)
 );
