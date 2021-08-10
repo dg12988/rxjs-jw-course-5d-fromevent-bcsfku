7d24c76500fbe3bb73f7830d6eb685524b6323e8
dg12988 <doug.goldberg@me.com>
2021-08-10 02:19:33 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -2,16 +2,22 @@ import { fromEvent, Observable } from "rxjs";
 
 const triggerButton = document.querySelector('button#trigger');
 
-// fromEvent<MouseEvent>(triggerButton, 'click').subscribe(
-//   event => console.log(event.type, event.x, event.y)
-// );
-
-const triggerClick$ = new Observable<MouseEvent>(subscriber => {
-  triggerButton.addEventListener('click', event => {
-    subscriber.next(event)
-  });
-});
-
-triggerClick$.subscribe(
+const subscription = fromEvent<MouseEvent>(triggerButton, 'click').subscribe(
   event => console.log(event.type, event.x, event.y)
 );
+
+
+setTimeout(() => {
+  console.log('Unsubscribe');
+  subscription.unsubscribe();
+}, 5000);
+
+// const triggerClick$ = new Observable<MouseEvent>(subscriber => {
+//   triggerButton.addEventListener('click', event => {
+//     subscriber.next(event)
+//   });
+// });
+
+// triggerClick$.subscribe(
+//   event => console.log(event.type, event.x, event.y)
+// );
